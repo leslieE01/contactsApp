86933a459bc09448804956b2258434f7d31bd3d6
leslieE01 <lehs112@hotmail.com>
2024-11-04 08:37:20 -0500

    SEM13-1

--- app/src/main/java/com/upn/contactsapp/services/ContactService.java
+++ app/src/main/java/com/upn/contactsapp/services/ContactService.java
@@ -9,11 +9,12 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface ContactService {
 
     @GET("/contacts")
-    Call< List<Contact> > getAll();
+    Call< List<Contact> > getAll(@Query("page") int page, @Query("limit") int limit);
 
     @GET("/contacts/{id}")
     Call< Contact > find(@Path("id") int id);
